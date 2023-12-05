01d8a0b3eae5340f83913c01d172fb99872f93d7
Mas281 <github@samdev.io>
2023-12-05 23:19:55 +0000

    Optimise day 5 even further

--- days/day05.ml
+++ days/day05.ml
@@ -32,27 +32,60 @@ let part_1 seeds maps =
   |> Option.value_exn
 ;;
 
+let apply_map_rule_to_range (src, dest, length) (range_start, range_end) =
+  let rule_start, rule_end = src, src + length in
+  let unmapped_before =
+    match range_start < rule_start with
+    | true -> Some (range_start, Int.min range_end rule_start)
+    | false -> None
+  in
+  let unmapped_after =
+    match range_end > rule_end with
+    | true -> Some (Int.max range_start rule_end, range_end)
+    | false -> None
+  in
+  let intersection =
+    match
+      (range_start >= rule_start && range_start <= rule_end)
+      || (range_end >= rule_start && range_end <= rule_end)
+    with
+    | true ->
+      let offset = dest - src in
+      Some (offset + Int.max range_start rule_start, offset + Int.min range_end rule_end)
+    | false -> None
+  in
+  let unmapped = [ unmapped_before; unmapped_after ] in
+  let mapped = [ intersection ] in
+  List.filter_opt unmapped, List.filter_opt mapped
+;;
+
+let apply_map_to_ranges ranges map =
+  let rec f unmapped mapped = function
+    | [] -> unmapped @ mapped
+    | rule :: rest ->
+      let new_unmapped, new_mapped =
+        List.map unmapped ~f:(apply_map_rule_to_range rule)
+        |> List.unzip
+        |> Tuple2.map ~f:List.concat
+      in
+      f new_unmapped (new_mapped @ mapped) rest
+  in
+  f ranges [] map
+;;
+
 let part_2 seeds maps =
   let seed_ranges =
     List.chunks_of seeds ~length:2
     |> List.map ~f:(function
-      | [ a; b ] -> a, b
+      | [ start; length ] -> start, start + length
       | _ -> failwith "odd length")
   in
-  let inverse_maps =
-    List.map maps ~f:(List.map ~f:(fun (src, dest, length) -> dest, src, length))
-    |> List.rev
+  let mapped_ranges = List.fold maps ~init:seed_ranges ~f:apply_map_to_ranges in
+  let min_location =
+    let compare = Comparable.lift Int.compare ~f:fst in
+    List.min_elt mapped_ranges ~compare |> Option.value_exn |> fst
   in
-  let rec find_lowest_seed location =
-    let seed = apply_range_maps ~init:location inverse_maps in
-    match
-      List.exists seed_ranges ~f:(fun (start, length) ->
-        seed >= start && seed < start + length)
-    with
-    | true -> location
-    | false -> find_lowest_seed (location + 1)
-  in
-  find_lowest_seed 0
+  min_location
 ;;
 
 let () =
