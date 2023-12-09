e1aa09f5238f0ef77c7eabfe30227e72c266296d
Mas281 <github@samdev.io>
2023-12-09 10:42:49 +0000

    Add day 9

---
+++ days/day09.ml
@@ -0,0 +1,27 @@
+open! Core
+open! Adventofcode.Parser
+open! Adventofcode.Utils
+
+let parse_sequences = some (list int ~sep:(char ' ') <* consume_line)
+
+let rec next_value sequence =
+  match List.for_all sequence ~f:(( = ) 0) with
+  | true -> 0
+  | false ->
+    let last = List.last_exn sequence in
+    let differences =
+      let except_last = List.take sequence (List.length sequence - 1) in
+      List.zip_exn except_last (List.tl_exn sequence) |> List.map ~f:(fun (a, b) -> b - a)
+    in
+    last + next_value differences
+;;
+
+let part_1 sequences = List.map sequences ~f:next_value |> List.reduce_exn ~f:( + )
+let part_2 sequences = List.map sequences ~f:List.rev |> part_1
+
+let () =
+  let input = read_input ~day:9 in
+  let sequences = run_exn parse_sequences input in
+  part_1 sequences |> Int.to_string |> print_endline;
+  part_2 sequences |> Int.to_string |> print_endline
+;;
